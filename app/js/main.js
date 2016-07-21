6002dbb77e30efb0c3bbbd6ef4a099d96bc330f3
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-07-22 01:31:57 +0200

    fix regression

--- app/js/main.js
+++ app/js/main.js
@@ -6,10 +6,21 @@
 
 (function($) {
 
+	skel
+	.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+
 	$(function() {
 
 		var	$window = $(window),
 			$body = $('body'),
+			$wrapper = $('#page-wrapper'),
 			$banner = $('#banner'),
 			$header = $('#header');
 
@@ -22,6 +33,29 @@
 				}, 100);
 			});
 
+		// Mobile?
+			if (skel.vars.mobile)
+				$body.addClass('is-mobile');
+			else
+				skel
+					.on('-medium !medium', function() {
+						$body.removeClass('is-mobile');
+					})
+					.on('+medium', function() {
+						$body.addClass('is-mobile');
+					});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
 		// Scrolly.
 			$('.scrolly')
 				.scrolly({
@@ -30,6 +64,8 @@
 				});
 
 		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
 
 			if ($banner.length > 0
 			&&	$header.hasClass('alt')) {
