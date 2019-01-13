860287036e6d56c8b247a83e75ccef3096545247
Samuel Martineau <martineau.samuel.1990@gmail.com>
2019-01-13 20:08:11 +0100

    improve lazy loading

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,20 +1,19 @@
 var paths = [
-  '/',
-  '/style/app.min.css',
-  '/js/app.min.js',
-  '/index.html',
-  '/images/banner.jpg',
-  '/images/pic01.jpg',
-  '/images/pic02.jpg',
-  '/images/pic03.jpg',
-  '/images/arrow.svg',
-  '/images/project/beerpongtournament.png',
-  '/images/project/giveittome.png',
-  '/images/project/gulp.png',
-  '/images/project/me.png',
-  '/images/project/merrycrosstmas.png'
+    "/",
+    "/index.html",
+    "/images/banner.jpg",
+    "/images/pic01.jpg",
+    "/images/pic02.jpg",
+    "/images/pic03.jpg",
+    "/images/arrow.svg",
+    "/images/project/beerpongtournament.png",
+    "/images/project/giveittome.png",
+    "/images/project/gulp.png",
+    "/images/project/me.png",
+    "/images/project/merrycrosstmas.png",
+    "/images/samuelmartineau.jpg"
 ];
 
-caches.open('offline-v1').then(function(cache) {
-  return cache.addAll(paths);
-})
+caches.open("offline-v2").then(function(cache) {
+    return cache.addAll(paths);
+});
