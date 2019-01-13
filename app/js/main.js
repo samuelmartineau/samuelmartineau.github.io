e17885430881ad9b466d29e8d50765332eb98c60
Samuel Martineau <martineau.samuel.1990@gmail.com>
2019-01-13 15:56:54 +0100

    lazy loading image

--- app/js/main.js
+++ app/js/main.js
@@ -5,111 +5,140 @@
 */
 
 (function($) {
-
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').then(function() {
-        // Success
-      }).catch(function() {
-        // Fail :(
-      });
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then(function() {
+                // Success
+            })
+            .catch(function() {
+                // Fail :(
+            });
     }
 
-	skel
-	.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+    skel.breakpoints({
+        xlarge: "(max-width: 1680px)",
+        large: "(max-width: 1280px)",
+        medium: "(max-width: 980px)",
+        small: "(max-width: 736px)",
+        xsmall: "(max-width: 480px)"
+    });
 
+    $(function() {
+        var $window = $(window),
+            $body = $("body"),
+            $wrapper = $("#page-wrapper"),
+            $banner = $("#banner"),
+            $header = $("#header");
 
-	$(function() {
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass("is-loading");
 
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#page-wrapper'),
-			$banner = $('#banner'),
-			$header = $('#header');
+        $window.on("load", function() {
+            window.setTimeout(function() {
+                $body.removeClass("is-loading");
+            }, 100);
+        });
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Mobile?
+        if (skel.vars.mobile) $body.addClass("is-mobile");
+        else
+            skel.on("-medium !medium", function() {
+                $body.removeClass("is-mobile");
+            }).on("+medium", function() {
+                $body.addClass("is-mobile");
+            });
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        // Fix: Placeholder polyfill.
+        $("form").placeholder();
 
-		// Mobile?
-			if (skel.vars.mobile)
-				$body.addClass('is-mobile');
-			else
-				skel
-					.on('-medium !medium', function() {
-						$body.removeClass('is-mobile');
-					})
-					.on('+medium', function() {
-						$body.addClass('is-mobile');
-					});
+        // Prioritize "important" elements on medium.
+        skel.on("+medium -medium", function() {
+            $.prioritize(
+                ".important\\28 medium\\29",
+                skel.breakpoint("medium").active
+            );
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Scrolly.
+        $(".scrolly").scrolly({
+            speed: 1500,
+            offset: $header.outerHeight()
+        });
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Header.
+        if (skel.vars.IEVersion < 9) $header.removeClass("alt");
 
-		// Scrolly.
-			$('.scrolly')
-				.scrolly({
-					speed: 1500,
-					offset: $header.outerHeight()
-				});
+        if ($banner.length > 0 && $header.hasClass("alt")) {
+            $window.on("resize", function() {
+                $window.trigger("scroll");
+            });
 
-		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
+            $banner.scrollex({
+                bottom: $header.outerHeight() + 1,
+                terminate: function() {
+                    $header.removeClass("alt");
+                },
+                enter: function() {
+                    $header.addClass("alt");
+                },
+                leave: function() {
+                    $header.removeClass("alt");
+                }
+            });
+        }
 
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
+        new IsoGrid(document.querySelector(".isolayer--deco4"), {
+            perspective: 3000,
+            transform:
+                "translate3d(-50px,-150px,0) scale3d(0.8,0.8,1) rotateY(45deg) rotateZ(-10deg)",
+            stackItemsAnimation: {
+                properties: function(pos) {
+                    return {
+                        rotateX: (pos + 1) * -15
+                    };
+                },
+                options: function(pos, itemstotal) {
+                    return {
+                        type: dynamics.spring,
+                        delay: (itemstotal - pos - 1) * 30
+                    };
+                }
+            },
+            onGridLoaded: function() {
+                classie.add(document.body, "grid-loaded");
+            }
+        });
+    });
 
-				$window.on('resize', function() { $window.trigger('scroll'); });
+    const images = document.querySelectorAll("img[data-js-lazy-image]");
+    const config = {
+        // If the image gets within 50px in the Y axis, start the download.
+        rootMargin: "50px 0px",
+        threshold: 0.01
+    };
+    function preloadImage(image) {
+        image.src = image.dataset.src;
+    }
+    function onIntersection(entries) {
+        // Loop through the entries
+        entries.forEach(entry => {
+            // Are we in viewport?
+            if (entry.intersectionRatio > 0) {
+                // Stop watching and load the image
+                observer.unobserve(entry.target);
+                preloadImage(entry.target);
+            }
+        });
+    }
 
-				$banner.scrollex({
-					bottom:		$header.outerHeight() + 1,
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); }
-				});
-
-			}
-
-			new IsoGrid(document.querySelector('.isolayer--deco4'), {
-				perspective: 3000,
-				transform : 'translate3d(-50px,-150px,0) scale3d(0.8,0.8,1) rotateY(45deg) rotateZ(-10deg)',
-				stackItemsAnimation : {
-					properties : function(pos) {
-						return {
-							rotateX: (pos+1) * -15
-						};
-					},
-					options : function(pos, itemstotal) {
-						return {
-							type: dynamics.spring,
-							delay: (itemstotal-pos-1)*30
-						};
-					}
-				},
-				onGridLoaded : function() {
-					classie.add(document.body, 'grid-loaded');
-				}
-			});
-
-	});
-
+    if (!("IntersectionObserver" in window)) {
+        Array.from(images).forEach(image => preloadImage(image));
+    } else {
+        // It is supported, load the images
+        observer = new IntersectionObserver(onIntersection, config);
+        images.forEach(image => {
+            observer.observe(image);
+        });
+    }
 })(jQuery);
