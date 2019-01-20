431656b3e8f64eed057622954c6d6e96bafcbc35
Samuel Martineau <martineau.samuel.1990@gmail.com>
2019-01-20 22:58:53 +0100

    pwa

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,19 +1,42 @@
-var paths = [
+var cacheStorageKey = "samuel-martineau-v1";
+var cacheList = [
     "/",
     "/index.html",
+    "/style/app.min.css",
+    "/js/app.min.js",
     "/images/banner.jpg",
     "/images/pic01.jpg",
     "/images/pic02.jpg",
     "/images/pic03.jpg",
     "/images/arrow.svg",
-    "/images/project/beerpongtournament.png",
-    "/images/project/giveittome.png",
-    "/images/project/gulp.png",
-    "/images/project/website.png",
-    "/images/project/merrycrosstmas.png",
+    "/images/projects/beerpongtournament.png",
+    "/images/projects/giveittome.png",
+    "/images/projects/gulp.png",
+    "/images/projects/website.png",
+    "/images/projects/merrycrosstmas.png",
     "/images/samuelmartineau.jpg"
 ];
 
-caches.open("offline-v2").then(function(cache) {
-    return cache.addAll(paths);
+self.addEventListener("install", function(e) {
+    console.log("Cache event!");
+    e.waitUntil(
+        caches
+            .open(cacheStorageKey)
+            .then(function(cache) {
+                console.log("Adding to Cache:", cacheList);
+                return cache.addAll(cacheList);
+            })
+            .then(function() {
+                console.log("Skip waiting!");
+                return self.skipWaiting();
+            })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
