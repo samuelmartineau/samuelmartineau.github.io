de71356666fec36c5e62ccce228233dabb11caf0
Samuel Martineau <martineau.samuel.1990@gmail.com>
2020-01-24 14:21:37 +0100

    chore: move to pug

--- brunch-config.js
+++ brunch-config.js
@@ -1,50 +1,41 @@
 exports.config = {
-  sourceMaps: !!process.env.DEV,
-  optimize: !process.env.DEV,
-  npm: {
-    enabled: false
-  },
-  server: {
-    run: true
-  },
-  modules: {
-    definition: false,
-    wrapper: false
-  },
-  paths: {
-    watched: ['app'],
-    public: 'public'
-  },
-  files: {
-    stylesheets: {
-      joinTo: {
-        'style/app.min.css': /^app\/sass\/main.scss/
-      }
+    sourceMaps: !!process.env.DEV,
+    optimize: !process.env.DEV,
+    npm: {
+        enabled: false
     },
-    javascripts: {
-      joinTo: 'js/app.min.js',
-      order: {
-        before: ['app/js/modernizr.custom.js'],
-        after: ['app/js/main.js']
-      }
-    }
-  },
-  conventions: {
-    assets: /static[\\\/]/
-  },
-  plugins: {
-    sass: {
-      mode: 'native'
+    server: {
+        run: true
     },
-    jadePages: {
-      pattern: /^app\/views\/pages\/.*\.jade$/,
-      destination: function(path) {
-        return path.replace(/^app[\/\\]views[\/\\]pages[\/\\](.*)\.jade$/, '$1.html');
-      },
-      jade: {
-        doctype: 'html'
-      },
-      htmlmin: true
+    modules: {
+        definition: false,
+        wrapper: false
+    },
+    paths: {
+        watched: ["app"],
+        public: "public"
+    },
+    files: {
+        stylesheets: {
+            joinTo: {
+                "style/app.min.css": /^app\/sass\/main.scss/
+            }
+        },
+        javascripts: {
+            joinTo: "js/app.min.js",
+            order: {
+                before: ["app/js/modernizr.custom.js"],
+                after: ["app/js/main.js"]
+            }
+        }
+    },
+    conventions: {
+        assets: /static[\\\/]/
+    },
+    plugins: {
+        sass: {
+            mode: "native"
+        },
+        pug: {}
     }
-  }
 };
